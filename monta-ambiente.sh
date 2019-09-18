7071a6a49b891eca2827105188fbd848986215c5
Ramon Mesquita <ramonmesquita113@gmail.com>
2019-09-18 10:37:43 -0300

    Renomeando pasta e ajuste chamada script monta-ambiente

--- monta-ambiente.sh
+++ monta-ambiente.sh
@@ -1,11 +1,10 @@
-./atualizador.sh
-./dependecias-comuns.sh
-./atualiza-bash.sh
-apt-get/./openjdk8.sh
-apt-get/./git.sh
-apt-get/./google-chrome.sh
-apt-get/./maven.sh
-apt-get/./docker.sh
-snap/./vscode.sh
-snap/./eclipse.sh
-snap/./postman.sh
+sh atualizador.sh
+sh dependecias-comuns.sh
+sh atualiza-bash.sh
+sh apt/openjdk8.sh
+sh apt/google-chrome.sh
+sh apt/maven.sh
+sh apt/yarn.sh
+sh apt/docker.sh
+sh apt/yarn.sh
+sh snap/postman.sh
